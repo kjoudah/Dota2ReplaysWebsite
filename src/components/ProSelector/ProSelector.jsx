3a04da0eb38cca1ebba6816611b084424ff8628b
Karam <karamjoudah@hotmail.com>
2023-10-29 23:38:53 -0400

    WIP - changed multiselect, button, and table - need to add filtering and sorting

--- src/components/ProSelector/ProSelector.jsx
+++ src/components/ProSelector/ProSelector.jsx
@@ -1,18 +1,21 @@
 'use client';
 
-import { MultiSelect, Loader, Flex, Group } from '@mantine/core';
+// import { MultiSelect, Loader, Flex, Group } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import useSWRMutation from 'swr/mutation';
 import ReplayResultsTable from '../ReplayResultsTable/ReplayResultsTable';
 import ReplayResultsTableMUI from '../ReplayResultsTable/ReplayResultsTableMUI';
 import MultipleSelect from '../MultipleSelect/MultipleSelect';
-import { Container, Stack, Button, LinearProgress } from '@mui/material';
+// import { Container, Stack, Button, LinearProgress } from '@mui/material';
+import { MultiSelect } from 'primereact/multiselect';
+import { Button } from 'primereact/button';
+import { ProgressBar } from 'primereact/progressbar';
+import ReplayResultsTablePrimereact from '../ReplayResultsTable/ReplayResultsTablePrimereact';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function ProSelector({ proList }) {
   const [selectedPros, setSelectedPros] = useState([]);
-
   const { trigger, data, isMutating } = useSWRMutation(
     `./replays?players=${selectedPros.join(',')}`,
     fetcher
@@ -23,44 +26,45 @@ export default function ProSelector({ proList }) {
       trigger();
     }
   }
-
-  return (
-    <Stack
-      width="100%"
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <MultipleSelect
+  const cities = [
+    { name: 'New York' },
+    { name: 'Rome' },
+    { name: 'London' },
+    { name: 'Istanbul' },
+    { name: 'Paris' },
+  ];
+  {
+    /* <MultipleSelect
           data={proList}
           onMultiSelectChange={data => {
             setSelectedPros(data);
           }}
-        ></MultipleSelect>
-        <Button
-          sx={{
-            p: 2,
-          }}
-          variant="contained"
-          onClick={() => getProPlayerReplays()}
-        >
-          Get Replays
-        </Button>
-      </Stack>
-      {isMutating && (
-        <LinearProgress
-          sx={{
-            width: 800,
-          }}
-        />
-      )}
-      {data && <ReplayResultsTableMUI data={data} />}
-    </Stack>
+        ></MultipleSelect> */
+  }
+
+  return (
+    <div className="w-full m-8 flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-row justify-center gap-4">
+          <MultiSelect
+            value={selectedPros}
+            onChange={e => setSelectedPros(e.value)}
+            options={proList}
+            placeholder="Select pros"
+            maxSelectedLabels={5}
+            className="w-96"
+          />
+          <Button label="Get Replays" onClick={() => getProPlayerReplays()} />
+        </div>
+        {isMutating && (
+          <ProgressBar
+            style={{ height: '2px', width: '100%' }}
+            mode="indeterminate"
+          ></ProgressBar>
+        )}
+      </div>
+
+      {data && <ReplayResultsTablePrimereact data={data} />}
+    </div>
   );
 }
